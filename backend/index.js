caeb021998256542da7127804b7993d4580c4b1a
panduramadhan2 <panduramadhan2@gmail.com>
2023-07-28 15:01:34 +0700

    ready deploy take to home netlify

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import Inventory from "./routes/InventoryRoute.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-mongoose.connect("mongodb://127.0.0.1:27017/inventory_db", {
+const connectionStr = process.env.MONGO_CONNECTION;
+// mongoose.connect("mongodb://127.0.0.1:27017/inventory_db", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(connectionStr, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Database Connected..."));
